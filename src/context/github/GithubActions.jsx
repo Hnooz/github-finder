d6c26cdaa2fa57a78d14defe3cfa6b6a1c1e2851
Hnooz <mohamededris23@gmail.com>
2023-02-13 09:20:24 +0200

    wip

--- src/context/github/GithubActions.jsx
+++ src/context/github/GithubActions.jsx
@@ -3,7 +3,7 @@ const GITHUB_URL = import.meta.env.VITE_GITHUB_URL
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN
 const github = axios.create({
     baseURL: GITHUB_URL,
-    headers: {Authorization: `token ${GITHUB_TOKEN}`}
+    headers: { Authorization: `token ${GITHUB_TOKEN}`}
 })
 
 export const searchUsers = async (text) => {
